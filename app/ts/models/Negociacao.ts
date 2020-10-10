23041d992a276682cbc0941575a5a29ff86a2526
Felipe Pereira <felipe.pereira@app4point.com.br>
2020-10-10 18:05:22 -0300

    Adiconado propriedade readonly tirando a necessidade de gets.

--- app/ts/models/Negociacao.ts
+++ app/ts/models/Negociacao.ts
@@ -1,23 +1,11 @@
 export class Negociacao {
   constructor(
-    private _data: Date,
-    private _quantidade: number,
-    private _valor: number
+    readonly data: Date,
+    readonly quantidade: number,
+    readonly valor: number
   ) {}
 
   get volume() {
-    return this._quantidade * this._valor;
-  }
-
-  get data() {
-    return new Date(this._data.getTime());
-  }
-
-  get quantidade() {
-    return this._quantidade;
-  }
-
-  get valor() {
-    return this._valor;
+    return this.quantidade * this.valor;
   }
 }
