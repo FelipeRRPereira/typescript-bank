6b052b47708f85bb1c489e968b34e830b987d594
Felipe Pereira <felipe.pereira@app4point.com.br>
2020-10-13 10:18:41 -0300

    Aplicando decorators sobre método update.

--- app/ts/views/View.ts
+++ app/ts/views/View.ts
@@ -1,17 +1,19 @@
+import { logarTempoDeExecucao } from "../helpers/decorators/index";
+
 export abstract class View<T> {
   private _elemento: JQuery;
   private _escape: boolean;
 
-  constructor(seletor: string, escape: boolean = false) {
+  constructor(seletor: string, escape = false) {
     this._elemento = $(seletor);
     this._escape = escape;
   }
 
+  @logarTempoDeExecucao(true)
   update(model: T): void {
     let template = this.template(model);
-    if(this._escape)
-      template = template.replace(/<script>[\s\S]*?<\/script>/g, '');
-    this._elemento.html(this.template(model));
+    if (this._escape) template = template.replace(/<script>[\s\S]*?<\/script>/g, "");
+    this._elemento.html(template);
   }
 
   abstract template(model: T): string;
