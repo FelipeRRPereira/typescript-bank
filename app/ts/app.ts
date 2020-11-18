f4840c680ffa54f856623ede9ef567455f6055f6
Felipe Pereira <felipe.pereira@app4point.com.br>
2020-11-17 21:26:41 -0300

    Adicionado botão importar.

--- app/ts/app.ts
+++ app/ts/app.ts
@@ -2,3 +2,4 @@ import { NegociacaoController } from "./controllers/NegociacaoController";
 
 const controller = new NegociacaoController();
 $(".form").submit(controller.adiciona.bind(controller));
+$("#botao-importa").on("click", controller.importarDados.bind(controller));
